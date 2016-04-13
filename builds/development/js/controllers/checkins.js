61c4e716effa1f6b842f2e3c9602ed20ea16bbde
IgorTokman <igortokman1994@ukr.net>
2016-04-14 00:30:32 +0300

    created a checkin method

--- builds/development/js/controllers/checkins.js
+++ builds/development/js/controllers/checkins.js
@@ -7,7 +7,25 @@
             function ($scope, $rootScope, $firebaseObject, $routeParams,
                       $firebaseArray, FIREBASE_URL) {
 
-                        
+                      $scope. whichmeeting = $routeParams.mId;
+                      $scope. whichuser = $routeParams.uId;
+
+                var ref = new Firebase(FIREBASE_URL + 'users/' +
+                $scope.whichuser + '/meetings/' + $scope.whichmeeting
+                + '/checkins');
+
+                $scope.addCheckin = function () {
+                    var checkinsInfo = $firebaseArray(ref);
+                    var myData= {
+                        firstname:  $scope.user.firstname,
+                        lastname:   $scope.user.lastname,
+                        email:      $scope.user.email,
+                        date:       Firebase.ServerValue.TIMESTAMP
+                    };     //myData
+
+                    checkinsInfo.$add(myData);
+                    $scope.user = {};
+                };      //addCheckin
 
             }]);   //Controller
 
